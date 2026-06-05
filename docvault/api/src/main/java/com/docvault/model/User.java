ac3a3a45411321cbb75aa51828f8688885c6c233
mateusaa <mateus.a.araujo2006@gmail.com>
2026-06-05 02:11:36 -0300

    setup Springboot + User entity

---
+++ docvault/api/src/main/java/com/docvault/model/User.java
@@ -0,0 +1,42 @@
+package com.docvault.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "users")
+public class User {
+    @Setter
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Getter
+    private  String name;
+    @Getter
+    private String email;
+    private String passwordHash;
+    @Getter @Setter
+    private String role;
+    @Getter @Setter
+    private Boolean active;
+    @Getter
+    private LocalDateTime createdAt;
+    @Getter
+    private  LocalDateTime updatedAt;
+    public  User(String name, String email, String passwordHash, String role, Boolean active) {
+
+        this.name = name;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.role = role;
+        this.active = active;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public User() {
+
+    }
+}
