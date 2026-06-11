6f314d47698112d78b05fd729c0ac0c4b22a0e8b
mateusaa <mateus.a.araujo2006@gmail.com>
2026-06-11 12:08:22 -0300

    refactor: change package path of tests and remove lombok from RegisterRequestDTO

--- docvault/api/src/main/java/com/edTech/dto/RegisterRequestDTO.java
+++ docvault/api/src/main/java/com/edTech/dto/RegisterRequestDTO.java
@@ -3,11 +3,6 @@ package com.edTech.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.Getter;
-import lombok.Setter;
-
-@Setter
-@Getter
 public class RegisterRequestDTO {
     @NotBlank(message = "Nome é obrigatório")
     private String name;
@@ -27,5 +22,27 @@ public class RegisterRequestDTO {
         this.password = password;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
