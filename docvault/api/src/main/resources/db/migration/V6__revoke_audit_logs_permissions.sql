d1c023ab5e8ec81c22a24636d0c22a021fc28d31
mateusaa <mateus.a.araujo2006@gmail.com>
2026-06-12 00:34:10 -0300

    feat: create function prevent_audit_logs_update

--- docvault/api/src/main/resources/db/migration/V6__revoke_audit_logs_permissions.sql
+++ docvault/api/src/main/resources/db/migration/V6__revoke_audit_logs_permissions.sql
@@ -1,12 +1,26 @@
+-- Revoga permissões, embora a proteção principal venha das triggers
 REVOKE UPDATE, DELETE ON TABLE audit_logs FROM edtech_user;
 
+-- Trigger para impedir DELETES
 CREATE OR REPLACE FUNCTION prevent_audit_logs_delete()
     RETURNS TRIGGER AS $$
 BEGIN
-    RAISE EXCEPTION 'Não é permitido deletar conteúdo da tabela audit_logs.';
+    RAISE EXCEPTION 'Não é permitido excluir conteúdos da tabela audit_logs.';
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER trg_prevent_audit_logs_delete
     BEFORE DELETE ON audit_logs
     FOR EACH ROW EXECUTE FUNCTION prevent_audit_logs_delete();
+
+-- Trigger para impedir UPDATES
+CREATE OR REPLACE FUNCTION prevent_audit_logs_update()
+    RETURNS TRIGGER AS $$
+BEGIN
+    RAISE EXCEPTION 'Não é permitido modificar a tabela audit_logs.';
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_prevent_audit_logs_update
+    BEFORE UPDATE ON audit_logs
+    FOR EACH ROW EXECUTE FUNCTION prevent_audit_logs_update();
