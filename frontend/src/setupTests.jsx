fa55e4c290bda89105027721b8d38d65d564a3ba
pedrohpsantos <phps2014.phps@gmail.com>
2026-07-07 12:17:41 -0300

    test: mock canvas context in setupTests to fix slow tests and remove Not implemented warnings

--- frontend/src/setupTests.jsx
+++ frontend/src/setupTests.jsx
@@ -5,6 +5,36 @@ import 'vitest-axe/extend-expect';
 
 expect.extend(matchers);
 
+// Mock do Canvas para evitar "Not implemented: HTMLCanvasElement's getContext() method" e atrasos
+HTMLCanvasElement.prototype.getContext = () => {
+  return {
+    fillRect: () => {},
+    clearRect: () => {},
+    getImageData: (x, y, w, h) => ({ data: new Array(w * h * 4) }),
+    putImageData: () => {},
+    createImageData: () => ([]),
+    setTransform: () => {},
+    drawImage: () => {},
+    save: () => {},
+    fillText: () => {},
+    restore: () => {},
+    beginPath: () => {},
+    moveTo: () => {},
+    lineTo: () => {},
+    closePath: () => {},
+    stroke: () => {},
+    translate: () => {},
+    scale: () => {},
+    rotate: () => {},
+    arc: () => {},
+    fill: () => {},
+    measureText: () => ({ width: 0 }),
+    transform: () => {},
+    rect: () => {},
+    clip: () => {},
+  };
+};
+
 // Mock do matchMedia (não existe no JSDOM)
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
